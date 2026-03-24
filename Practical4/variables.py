d2bc3ce3b73c5b4306c4e463d390bf397ebea53d
Ly-Steve <yuxuan.25@intl.zju.edu.cn>
2026-03-24 10:41:30 +0800

    Practical4 finished

---
+++ Practical4/variables.py
@@ -0,0 +1,32 @@
+# Population of Scotland in 2004 (millions)
+a = 5.08
+# Population of Scotland in 2014 (millions)
+b = 5.33
+# Population of Scotland in 2024 (millions)
+c = 5.55
+
+# Calculate population change
+change_2004_2014 = b - a
+change_2014_2024 = c - b
+
+# Print results
+print("Population change 2004-2014:", change_2004_2014)
+print("Population change 2014-2024:", change_2014_2024)
+
+# Conclusion
+# Result: 0.25 > 0.22, the population growth rate of Scotland is slowing down
+
+
+# Boolean Values Task
+X = True
+Y = False
+
+# Logical OR operation
+W = X or Y
+print("Result of X or Y:", W)
+
+# Truth table for W (X or Y)
+# True  or  True  → True
+# True  or  False → True
+# False or  True  → True
+# False or  False → False
